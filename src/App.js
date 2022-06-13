bd24a331628bfca645463444d1cea04217e68b8f
Patrycja-dz <dziemianko.pat@gmail.com>
2022-06-13 15:56:09 +0200

    added Gallery component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
-
+import Gallery from './components/Gallery';
 function App() {
   return (
-    <div className="App">
- 
+    <div className='container'>
+         <section className='gallery-container'>
+        <Gallery/>
+      </section>
     </div>
   );
 }
-
+ 
 export default App;
